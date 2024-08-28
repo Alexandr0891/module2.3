32a01a983f6a0c26f536f1dfc309c98ea8a9597c
Alexandr0891 <alexmalcev1401@gmail.com>
2024-08-28 17:51:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#################################
+
+
+#Module 2 4
+#"Цикл for. Элементы списка. Полезные функции в цикле"
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = [   ]
+not_primes = [    ]
